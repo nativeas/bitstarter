7efa5871f9b6d56c83802520408470f84ca2cc1d
Nativeas <saicn@qq.com>
2013-07-16 12:55:49 +0800

    Update grader.js

--- grader.js
+++ grader.js
@@ -1,10 +1,29 @@
 #!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
 
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require("restler");
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -17,10 +36,6 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var assertURLExists =function(inUrl){
-    var instr = inUrl.toString();
-
-}
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -40,20 +55,6 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-var checkRemoteHtmlFile = function(url,checksfile){
-  rest.get(url.toString()).on("complete",function(result,response){
-    var cheio =cheerio.load(response)
-    var checks=loadChecks(checksfile).sort();
-    var  out={};
-    for (var ii in checks){
-      var present = cheio(checks[ii]).length>0;
-      out[checks[ii]] = present;
-    }
-    console.log(out)
-    return out;
-  })
-}
-
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -63,12 +64,11 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-u, --url <url_file>','URL to FIle')
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
-    
-    var checkJson =checkRemoteHtmlFile(program.url, program.checks);
-    //var outJson = JSON.stringify(checkJson, null, 4);
-   // console.log(outJson);
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
